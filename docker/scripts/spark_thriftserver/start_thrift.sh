7d0e93507557ed893af5b4289cec824e2f5ded2b
atans <atanskiy1989@gmail.com>
2025-12-09 09:30:11 +0200

    slight fixes

--- docker/scripts/spark_thriftserver/start_thrift.sh
+++ docker/scripts/spark_thriftserver/start_thrift.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Starting Spark Thrift Server in LOCAL mode..."
 
-sleep 10
+sleep 5
 
 /opt/spark/bin/spark-submit \
   --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 \
@@ -12,7 +12,7 @@ sleep 10
   --conf hive.server2.thrift.port=${HIVE_SERVER2_THRIFT_PORT} \
   --conf hive.server2.authentication=${HIVE_SERVER2_THRIFT_AUTHENTICATION} \
   --conf hive.metastore.uris=${THRIFT_HIVE_METASTORE} \
-  --conf spark.hadoop.fs.s3a.endpoint${S3_ENDPOINT_URL} \
+  --conf spark.hadoop.fs.s3a.endpoint=${S3_ENDPOINT_URL} \
   --conf spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID} \
   --conf spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY} \
   --conf spark.hadoop.fs.s3a.path.style.access=true
